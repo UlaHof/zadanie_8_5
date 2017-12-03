a8bf8e9e26e0fd9eac3bd9336a1292713eafe526
Ula <chcetylkoobejrzecfilm@gmail.com>
2017-12-03 23:04:15 +0100

    Add html structure and JavaScript file

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+var a  = prompt('Enter triangle base: '),
+    h = prompt('Enter triangle height: '),
+    triangleArea;
+triangleArea = a * h /2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea +'.');
